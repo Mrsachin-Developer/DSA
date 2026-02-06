c5a0b56bc4ba21e6e078654125cb0f0ac2654f01
Sabya Sachin Mohanta <tksfriday2024@gmail.com>
2026-02-06 20:41:32 +0530

    Time: 0 ms (100%), Space: 43 MB (66.76%) - LeetHub

---
+++ 0058-length-of-last-word/0058-length-of-last-word.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int lengthOfLastWord(String s) {
+        s = s.trim();  
+
+        int count = 0;
+        int n = s.length();
+
+        for (int i = n - 1; i >= 0; i--) {
+            if (s.charAt(i) == ' ') { 
+                break;
+            } else {
+                count++;
+            }
+        }
+        return count;
+    }
+}
