e820dcd679b6e9cc987bf26045d83481c70ca14d
Sabya Sachin Mohanta <tksfriday2024@gmail.com>
2026-03-12 07:12:08 +0530

    Time: 7 ms (98.75%), Space: 49.3 MB (37.05%) - LeetHub

---
+++ 0056-merge-intervals/0056-merge-intervals.java
@@ -0,0 +1,30 @@
+class Solution {
+    public int[][] merge(int[][] intervals) {
+
+        List<int[]> result = new ArrayList<>();
+
+        Arrays.sort(intervals, (a, b) -> a[0] - b[0]);
+        int start = intervals[0][0];
+        int end = intervals[0][1];
+
+        for (int i = 1; i < intervals.length; i++) {
+
+            int nextstart = intervals[i][0];
+            int nextend = intervals[i][1];
+
+            if (nextstart <= end) {
+                end = Math.max(end, nextend);
+
+            } else {
+                result.add(new int[] { start, end });
+                start = nextstart;
+                end = nextend;
+            }
+
+        }
+        result.add(new int[] { start, end });
+
+        return result.toArray(new int[result.size()][]);
+
+    }
+}
