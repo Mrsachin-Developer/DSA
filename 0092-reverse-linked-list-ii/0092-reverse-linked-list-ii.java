fada8de10c5a2f0ddcc5f29b14dcae00272b72de
Sabya Sachin Mohanta <tksfriday2024@gmail.com>
2026-03-20 06:57:10 +0530

    Time: 0 ms (100%), Space: 43.2 MB (41.7%) - LeetHub

--- 0092-reverse-linked-list-ii/0092-reverse-linked-list-ii.java
+++ 0092-reverse-linked-list-ii/0092-reverse-linked-list-ii.java
@@ -1,36 +1,33 @@
 class Solution {
-    public ListNode reverseBetween(ListNode head, int left, int right){
+    public ListNode reverseBetween(ListNode head, int left, int right) {
 
+        ListNode curr = head;
+        ListNode prev = null;
 
+        for (int i = 1; i < left; i++) {
+            prev = curr;
+            curr = curr.next;
+        }
 
+        ListNode connection = prev; // node before left
+        ListNode tail = curr; // will become tail after reverse
+        ListNode next = null;
 
-ListNode curr= head;
-ListNode prev=null;
+        for (int i = 0; i < right - left + 1; i++) {
+            next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+        if (connection != null) {
+            connection.next = prev;
+        } else {
+            head = prev;
+        }
 
-for(int i=1;i<left;i++){
-prev=curr;
-curr=curr.next;
-}
+        tail.next = curr;
 
- ListNode connection = prev; // node before left
-    ListNode tail = curr;       // will become tail after reverse
-ListNode next=null;
-
-for(int i=0;i<right-left+1;i++){
-    next =curr.next;
-    curr.next=prev;
-    prev=curr;
-    curr=next;
-}
-if (connection != null) {
-        connection.next = prev;
-    } else {
-        head = prev; 
-    }
-
-    tail.next = curr;
-
-    return head;
+        return head;
 
     }
 };
