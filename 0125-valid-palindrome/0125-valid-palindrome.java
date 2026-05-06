6e3150d6fa5b972c60e6ae16c694bc2163856071
Sabya Sachin Mohanta <tksfriday2024@gmail.com>
2026-05-06 09:28:01 +0530

    Time: 16 ms (12.92%), Space: 46.3 MB (31.1%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -1,18 +1,17 @@
 class Solution {
     public boolean isPalindrome(String s) {
-      s=s.toLowerCase().replaceAll("[^a-zA-Z0-9]", "");
+              s = s.toLowerCase().replaceAll("[^a-zA-Z0-9]", "");
 
         int left = 0;
         int right = s.length() - 1;
+
         while (left < right) {
-            if (s.isEmpty()) 
-                return true;
+            if (s.isEmpty()) return true;
             
             if (s.charAt(left) != s.charAt(right)) {
-            
                 return false;
             } else {
-                    left++;
+                left++;
                 right--;
             }
         }
