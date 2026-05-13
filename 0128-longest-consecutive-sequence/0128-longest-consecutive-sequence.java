e31d49b5147cdbb33f2f955c62f00856303d92de
Sabya Sachin Mohanta <tksfriday2024@gmail.com>
2026-05-13 20:13:58 +0530

    Time: 31 ms (65.11%), Space: 94.9 MB (70.53%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.java
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.java
@@ -25,7 +25,7 @@
 //             }
 //         }
 //         return longest;
-        
+
 //     }
 // }
 
@@ -34,18 +34,16 @@ import java.util.*;
 class Solution {
     public int longestConsecutive(int[] nums) {
 
-        if (nums.length == 0) return 0;
+        if (nums.length == 0)
+            return 0;
 
         HashSet<Integer> set = new HashSet<>();
 
         for (int num : nums) {
             set.add(num);
         }
-
         int longestLength = 0;
-
         for (int num : set) {
-
             if (!set.contains(num - 1)) {
 
                 int currentNum = num;
@@ -55,7 +53,6 @@ class Solution {
                     currentNum++;
                     currentLength++;
                 }
-
                 longestLength = Math.max(longestLength, currentLength);
             }
         }
