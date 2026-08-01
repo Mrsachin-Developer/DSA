661c23a351e1b25a115bd5baf114f3d9c9d828de
Sabya Sachin Mohanta <tksfriday2024@gmail.com>
2026-08-01 23:10:18 +0530

    Time: 3 ms (86.41%), Space: 47.8 MB (32.34%) - LeetHub

--- 0135-candy/0135-candy.java
+++ 0135-candy/0135-candy.java
@@ -1,33 +1,30 @@
 class Solution {
     public int candy(int[] ratings) {
-        int n= ratings.length;
-int[] candies= new int[n];
+        int n = ratings.length;
 
-Arrays.fill(candies,1);
+        int[] left = new int[n];
+        left[0] = 1;
+        for (int i = 1; i < n; i++) {
+            if (ratings[i] > ratings[i - 1]) {
+                left[i] = left[i - 1] + 1;
+            } else {
+                left[i] = 1;
+            }
+        }
+        int current = 1;
+        int right = 1;
+        int sum = left[n - 1];
+        for (int i = n - 2; i >= 0; i--) {
+            if (ratings[i] > ratings[i + 1]) {
+                current = right + 1;
+                right = current;
+            } else {
+                current = 1;
+                right=1;
+            }
+            sum = sum + Math.max(left[i], current);
 
-
-int left=0;
-
-//left to right
-for(int i=1;i<n;i++){
-    if (ratings[i]>ratings[i-1]) {
-        candies[i]=candies[i-1]+1;
-    }
-}
-
-
-for(int i=n-2;i>=0;i--){
-    if(ratings[i]>ratings[i+1]){
-        candies[i]= Math.max(candies[i],candies[i+1]+1); // suppose the candies are same  1,1 and the rating of i is > i+1 so we need the i to be greater 
-    }
-}
-
-int total=0;
-
-for(int c:candies){
-    total+=c;
-}
-
-return total;
+        }
+        return sum;
     }
 }
