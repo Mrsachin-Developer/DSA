6ae7f72d47c7a9ccdf7353588c9b21d26a7c8753
Sabya Sachin Mohanta <tksfriday2024@gmail.com>
2026-08-06 20:45:36 +0530

    Time: 5 ms (34.13%), Space: 45.7 MB (33.97%) - LeetHub

--- 0120-triangle/0120-triangle.java
+++ 0120-triangle/0120-triangle.java
@@ -2,13 +2,13 @@ class Solution {
 
     public int solve(List<List<Integer>> mat, int[][] dp, int r, int c) {
         int n = mat.size();
-        for (int j =0; j < n; j++) {
+        for (int j = 0; j < n; j++) {
             dp[n - 1][j] = mat.get(n - 1).get(j);
         }
         for (int i = n - 2; i >= 0; i--) {
             for (int j = i; j >= 0; j--) {
-                int d = mat.get(i).get(j) + dp[i+1][j];
-                int dg = mat.get(i).get(j) + dp[i+1][j+1];
+                int d = mat.get(i).get(j) + dp[i + 1][j];
+                int dg = mat.get(i).get(j) + dp[i + 1][j + 1];
                 dp[i][j] = Math.min(d, dg);
             }
         }
@@ -20,7 +20,6 @@ class Solution {
         int n = triangle.size();
         int[][] dp = new int[n][n];
 
-       
         return solve(triangle, dp, 0, 0);
     }
 }
