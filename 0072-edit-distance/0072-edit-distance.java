88aec94669366d2ca53a8335c2774413b7cecabd
Sabya Sachin Mohanta <tksfriday2024@gmail.com>
2026-08-18 12:32:19 +0530

    Time: 5 ms (66.38%), Space: 46.9 MB (83.99%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.java
+++ 0072-edit-distance/0072-edit-distance.java
@@ -7,7 +7,6 @@ class Solution {
 
         for (int i = 1; i <= n; i++) {
             for (int j = 1; j <= m; j++) {
-
                 if (s1.charAt(i-1) == s2.charAt(j-1)) {
                     dp[i][j] = 0 + dp[i - 1][j - 1];
                 } else
