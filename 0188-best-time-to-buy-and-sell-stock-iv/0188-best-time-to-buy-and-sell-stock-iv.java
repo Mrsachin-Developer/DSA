974a82a457423851879718912999aeb5e866aff7
Sabya Sachin Mohanta <tksfriday2024@gmail.com>
2026-08-19 10:17:18 +0530

    Time: 2 ms (71.63%), Space: 43 MB (94.68%) - LeetHub

--- 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.java
+++ 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.java
@@ -1,36 +1,29 @@
 class Solution {
     public int maxProfit(int k, int[] prices) {
         int n = prices.length;
-        int[][][] dp = new int[n+1][2][k+1];
+        int[][] ahead = new int[2][k + 1];
+        int[][] curr = new int[2][k + 1];
 
-        for (int buy = 0; buy <= 1; buy++) {
-            for (int cap = k; cap >= 0; cap--) {
-                dp[n][buy][cap] = 0;
-            }
-        }
-        for (int i = 0; i <= n; i++) {
-            for (int buy = 0; buy <= 1; buy++) {
-                dp[i][buy][0] = 0;
-            }
-        }
+ 
 
         for (int i = n - 1; i >= 0; i--) {
             for (int buy = 0; buy <= 1; buy++) {
                 for (int cap = k; cap >= 1; cap--) {
                     if (buy == 1) {
-                        int take = -prices[i] + dp[i + 1][0][cap];
-                        int nottake = dp[i + 1][1][cap];
+                        int take = -prices[i] + ahead[0][cap];
+                        int nottake =ahead[1][cap];
 
-                        dp[i][buy][cap] = Math.max(take, nottake);
+                     curr[buy][cap] = Math.max(take, nottake);
                     } else {
-                        int sell = prices[i] + dp[i + 1][1][cap - 1];
-                        int not = dp[i + 1][0][cap];
+                        int sell = prices[i] + ahead[1][cap - 1];
+                        int not = ahead[0][cap];
 
-                        dp[i][buy][cap] = Math.max(sell, not);
+                       curr[buy][cap] = Math.max(sell, not);
                     }
                 }
             }
+            ahead=curr.clone();
         }
-        return dp[0][1][k];
+        return ahead[1][k];
     }
 }
