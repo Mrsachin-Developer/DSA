044630d60022421f48e85008a7e34e3a90b05008
Sabya Sachin Mohanta <tksfriday2024@gmail.com>
2026-08-23 21:02:44 +0530

    Time: 1 ms (97.39%), Space: 47.8 MB (25.45%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
@@ -3,25 +3,40 @@ class Solution {
 
         int r = matrix.length;
         int c = matrix[0].length;
-        int[] col = new int[c];
-        int[] row = new int[r];
+        int col0 = 1;
         for (int i = 0; i < r; i++) {
             for (int j = 0; j < c; j++) {
 
                 if (matrix[i][j] == 0) {
-
-                    row[i] = 1;
-                    col[j] = 1;
+                    // ith row
+                    matrix[i][0] = 0;
+                    // ensure that the ist box is not used as we are using it for row
+                    if (j != 0) {
+                        matrix[0][j] = 0;
+                    } else {
+                        col0 = 0;
+                    }
                 }
             }
         }
 
-        for (int i = 0; i < r; i++) {
-            for (int j = 0; j < c; j++) {
-             if(row[i] == 1 || col[j] == 1) {
+        for (int i = 1; i < r; i++) {
+            for (int j = 1; j < c; j++) {
+                if (matrix[i][0] == 0 || matrix[0][j] == 0) {
                     matrix[i][j] = 0;
                 }
 
+            }
+        }
+
+        if(matrix[0][0]==0){
+            for(int j=0;j<c;j++){
+                matrix[0][j]=0;
+            }
+        }
+        if(col0==0){
+            for(int i=0;i<r;i++){
+                matrix[i][0]=0;
             }
         }
     }
