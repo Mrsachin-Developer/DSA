1c7e1a8f014d6b8b1a9ceaec43621d99a1c12bb1
Sabya Sachin Mohanta <tksfriday2024@gmail.com>
2026-08-24 11:51:19 +0530

    Time: 0 ms (100%), Space: 44.8 MB (33.5%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.java
+++ 0031-next-permutation/0031-next-permutation.java
@@ -1,54 +1,37 @@
 class Solution {
-
-    public void reverse(int[] nums, int start, int end){
-
-        while(start < end){
-
-            int temp = nums[start];
-            nums[start] = nums[end];
-            nums[end] = temp;
-
+    public void reverse(int[] a,int start,int end){
+        while(start<end){
+            int temp=a[start];
+            a[start]=a[end];
+            a[end]=temp;
             start++;
             end--;
         }
     }
-
     public void nextPermutation(int[] nums) {
-
         int n = nums.length;
-
         int index = -1;
-
-        for(int i = n-2; i >= 0; i--){
-
-            if(nums[i] < nums[i+1]){
-
+        for (int i = n - 2; i >= 0; i--) {
+            if (nums[i] < nums[i + 1]) {
                 index = i;
                 break;
             }
         }
 
-        // Last permutation
-        if(index == -1){
-
-            reverse(nums, 0, n-1);
+        if (index == -1) {
+            reverse(nums, 0, n - 1);
             return;
         }
 
-        // Find next greater element
-        for(int i = n-1; i > index; i--){
-
-            if(nums[i] > nums[index]){
-
+        for (int i = n - 1; i >index; i--) {
+            if (nums[i] > nums[index]) {
                 int temp = nums[i];
                 nums[i] = nums[index];
                 nums[index] = temp;
-
-                break;
+            break;
             }
         }
 
-        //  Reverse remaining part
-        reverse(nums, index+1, n-1);
+        reverse(nums,index+1,n-1);
     }
 }
