6ec571b15cf8a0b28c9d3203f5bec59b0854c823
Sabya Sachin Mohanta <tksfriday2024@gmail.com>
2026-08-31 12:24:20 +0530

    Time: 2 ms (89.4%), Space: 44.4 MB (13.84%) - LeetHub

---
+++ 0994-rotting-oranges/0994-rotting-oranges.java
@@ -0,0 +1,84 @@
+import java.util.*;
+
+class Pair {
+    int row;
+    int col;
+    int tm;
+
+    Pair(int row, int col, int tm) {
+        this.row = row;
+        this.col = col;
+        this.tm = tm;
+    }
+}
+
+class Solution {
+    public int orangesRotting(int[][] grid) {
+
+        int n = grid.length;
+        int m = grid[0].length;
+
+        Queue<Pair> q = new LinkedList<>();
+        int[][] vis = new int[n][m];
+
+        int cntFresh = 0;
+
+        // Add all rotten oranges to queue
+        // Count fresh oranges
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+
+                if (grid[i][j] == 2) {
+                    q.offer(new Pair(i, j, 0));
+                    vis[i][j] = 2;
+                } else {
+                    vis[i][j] = 0;
+                }
+
+                if (grid[i][j] == 1) {
+                    cntFresh++;
+                }
+            }
+        }
+
+        int tm = 0;
+        int cnt = 0;
+
+        int[] drow = {-1, 0, 1, 0};
+        int[] dcol = {0, 1, 0, -1};
+
+        while (!q.isEmpty()) {
+
+            Pair curr = q.poll();
+
+            int r = curr.row;
+            int c = curr.col;
+            int t = curr.tm;
+
+            tm = Math.max(tm, t);
+
+            for (int i = 0; i < 4; i++) {
+
+                int nrow = r + drow[i];
+                int ncol = c + dcol[i];
+
+                if (nrow >= 0 && nrow < n &&
+                    ncol >= 0 && ncol < m &&
+                    vis[nrow][ncol] != 2 &&
+                    grid[nrow][ncol] == 1) {
+
+                    q.offer(new Pair(nrow, ncol, t + 1));
+
+                    vis[nrow][ncol] = 2; // mark rotten
+                    cnt++;               // fresh orange rotted
+                }
+            }
+        }
+
+        if (cnt != cntFresh) {
+            return -1;
+        }
+
+        return tm;
+    }
+}
