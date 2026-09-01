56d828df5830dee51d6acfc5fd74d2fe0895ed6d
Sabya Sachin Mohanta <tksfriday2024@gmail.com>
2026-09-01 22:55:41 +0530

    Time: 2 ms (75.73%), Space: 47.4 MB (92.98%) - LeetHub

--- 0130-surrounded-regions/0130-surrounded-regions.java
+++ 0130-surrounded-regions/0130-surrounded-regions.java
@@ -1,69 +1,62 @@
 class Solution {
 
-    public void dfs(int ro, int col, int[][] vist, char[][] board) {
-
-        vist[ro][col] = 1;
-
-        int[] delRow = {-1, 0, 1, 0};
-        int[] delCol = {0, 1, 0, -1};
-
+    public void dfs(int row, int col, int[][] visited, char[][] board) {
         int n = board.length;
         int m = board[0].length;
+   int[] drow = {-1, 0, 1, 0};
+int[] dcol = {0, 1, 0, -1};
+
+        visited[row][col] = 1;
 
         for (int i = 0; i < 4; i++) {
+            int nrow = row + drow[i];
+            int ncol = col + dcol[i];
 
-            int nrow = ro + delRow[i];
-            int ncol = col + delCol[i];
-
-            if (nrow >= 0 && nrow < n &&
-                ncol >= 0 && ncol < m &&
-                vist[nrow][ncol] == 0 &&
-                board[nrow][ncol] == 'O') {
-
-                dfs(nrow, ncol, vist, board);
+            if (nrow >= 0 && nrow < n && ncol < m && ncol >= 0 && board[nrow][ncol] == 'O'
+                    && visited[nrow][ncol] == 0) {
+                dfs(nrow, ncol, visited, board);
             }
         }
+
     }
 
     public void solve(char[][] board) {
-
         int n = board.length;
         int m = board[0].length;
+        int[][] visited = new int[n][m];
 
-        int[][] vist = new int[n][m];
-
-        // Top and Bottom Row
         for (int j = 0; j < m; j++) {
+            //  1st row
+            if (board[0][j] == 'O' && visited[0][j] == 0) {
+                dfs(0, j, visited, board);
 
-            if (board[0][j] == 'O' && vist[0][j] == 0) {
-                dfs(0, j, vist, board);
             }
+            // last row
 
-            if (board[n - 1][j] == 'O' && vist[n - 1][j] == 0) {
-                dfs(n - 1, j, vist, board);
+            if (board[n - 1][j] == 'O' && visited[n - 1][j] == 0) {
+                dfs(n - 1, j, visited, board);
             }
         }
 
-        // Left and Right Column
         for (int i = 0; i < n; i++) {
+            //  1st col
+          if (board[i][0] == 'O' && visited[i][0] == 0) {
+                dfs(i, 0, visited, board);
 
-            if (board[i][0] == 'O' && vist[i][0] == 0) {
-                dfs(i, 0, vist, board);
             }
-
-            if (board[i][m - 1] == 'O' && vist[i][m - 1] == 0) {
-                dfs(i, m - 1, vist, board);
+            // last col
+            if (board[i][m - 1] == 'O' && visited[i][m - 1] == 0) {
+                dfs(i, m - 1, visited, board);
             }
         }
 
-        // Flip unvisited O to X
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < m; j++) {
-
-                if (board[i][j] == 'O' && vist[i][j] == 0) {
+                if (visited[i][j] == 0 && board[i][j] == 'O') {
                     board[i][j] = 'X';
                 }
             }
         }
+
     }
 }
