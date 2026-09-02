cbcdc9a6765fee0f13aa1567aa138361fbb3b225
Sabya Sachin Mohanta <tksfriday2024@gmail.com>
2026-09-02 21:14:16 +0530

    Time: 94 ms (33.14%), Space: 48.5 MB (37.23%) - LeetHub

---
+++ 0127-word-ladder/0127-word-ladder.java
@@ -0,0 +1,52 @@
+class Pair {
+    String first;
+    int second;
+
+    Pair(String first, int second) {
+        this.first = first;
+        this.second = second;
+    }
+}
+
+class Solution {
+    public int ladderLength(String beginWord, String endWord, List<String> wordList) {
+        Queue<Pair> q = new LinkedList<>();
+        Set<String> st = new HashSet<>();
+
+        int len = wordList.size();
+
+        q.offer(new Pair(beginWord, 1));
+
+        for (int i = 0; i < len; i++) {
+            st.add(wordList.get(i));
+        }
+
+        st.remove(beginWord);
+
+        while (!q.isEmpty()) {
+
+            String word = q.peek().first;
+            int steps = q.peek().second;
+            q.poll();
+            if (word.equals(endWord) == true) {
+                return steps;
+            }
+            for (int i = 0; i < word.length(); i++) {
+
+                for (char ch = 'a'; ch <= 'z'; ch++) {
+
+                    char replacedCharArray[] = word.toCharArray();
+                    replacedCharArray[i] = ch;
+                    String replacedWord = new String(replacedCharArray);
+
+                    if (st.contains(replacedWord) == true) {
+                        st.remove(replacedWord);
+                        q.offer(new Pair(replacedWord, steps + 1));
+                    }
+                }
+            }
+
+        }
+        return 0;
+    }
+}
