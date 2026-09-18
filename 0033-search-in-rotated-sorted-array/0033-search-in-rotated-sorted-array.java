ee46870c22e5faec05a876b826921ed0315a8585
Sabya Sachin Mohanta <tksfriday2024@gmail.com>
2026-09-18 12:08:18 +0530

    Time: 0 ms (100%), Space: 44 MB (28.22%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -10,9 +10,9 @@ class Solution {
             if (nums[mid] == target) {
                 return mid;
             } else if (nums[low] <= nums[mid]) {
+
                 if (target >= nums[low] && target < nums[mid]) {
                     high = mid - 1;
-
                 } else {
                     low = mid + 1;
                 }
@@ -21,7 +21,6 @@ class Solution {
 
                 if (target >= nums[mid] && target <= nums[high]) {
                     low = mid + 1;
-
                 } else {
                     high = mid - 1;
                 }
