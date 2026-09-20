e74730cbeca7d5943a58574b645d579699b732a8
Sabya Sachin Mohanta <tksfriday2024@gmail.com>
2026-09-20 11:02:43 +0530

    Time: 33 ms (69.8%), Space: 99.6 MB (87.5%) - LeetHub

--- 0155-min-stack/0155-min-stack.java
+++ 0155-min-stack/0155-min-stack.java
@@ -1,44 +1,34 @@
 class MinStack {
- Stack<Integer> mainStack;
-  Stack<Integer> minStack ;
+
+    Stack<Integer> mainStack;
+    Stack<Integer> minStack;
+
     public MinStack() {
-         mainStack = new Stack<>();
-      minStack = new Stack<>();
+        mainStack = new Stack<>();
+        minStack = new Stack<>();
     }
 
     public void push(int val) {
-
         mainStack.push(val);
 
-        if (minStack.isEmpty() || val <= minStack.peek())
+        if (minStack.isEmpty() || val <= minStack.peek()) {
             minStack.push(val);
+        }
     }
 
     public void pop() {
+        if (mainStack.peek().equals(minStack.peek())) {
+            minStack.pop();
+        }
 
-
-
-if(mainStack.peek().equals(minStack.peek())){ // Stack<Integer> stores Integer objects, not primitive int.  SO you will use == it compare the memory reference but .equls compare the val,if you use == you will get an wrong answer
-    minStack.pop();
-}
-mainStack.pop();
+        mainStack.pop();
     }
 
     public int top() {
- return mainStack.peek();
-
+        return mainStack.peek();
     }
 
     public int getMin() {
-  return minStack.peek();
+        return minStack.peek();
     }
 }
-
-/**
- * Your MinStack object will be instantiated and called as such:
- * MinStack obj = new MinStack();
- * obj.push(val);
- * obj.pop();
- * int param_3 = obj.top();
- * int param_4 = obj.getMin();
- */
