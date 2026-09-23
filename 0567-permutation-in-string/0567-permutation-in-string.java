4c929cc3abf1f5c44cf0c5a0cfb7de93447d3551
Sabya Sachin Mohanta <tksfriday2024@gmail.com>
2026-09-23 19:42:48 +0530

    Time: 7 ms (66.17%), Space: 43.7 MB (92.83%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.java
+++ 0567-permutation-in-string/0567-permutation-in-string.java
@@ -1,7 +1,7 @@
 class Solution {
     public boolean checkInclusion(String s1, String s2) {
         int freq[] = new int[26];
-        int [] windowfreq = new int[26];
+        int[] windowfreq = new int[26];
 
         for (int i = 0; i < s1.length(); i++) {
             char ch = s1.charAt(i);
