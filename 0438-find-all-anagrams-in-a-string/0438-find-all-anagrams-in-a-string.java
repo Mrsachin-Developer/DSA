b7d4b84c1ada40348633d45634b431d53efa32b1
Sabya Sachin Mohanta <tksfriday2024@gmail.com>
2026-09-23 19:56:02 +0530

    Time: 9 ms (92.09%), Space: 46.5 MB (95.57%) - LeetHub

--- 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.java
+++ 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.java
@@ -1,36 +1,26 @@
 class Solution {
     public List<Integer> findAnagrams(String s, String p) {
-        int[] freqMap = new int[26];
-        int[] windowFreq = new int[26];
         List<Integer> ans = new ArrayList<>();
-        int k = p.length();
-        int left = 0;
+        int[] freq = new int[26];
+
+        int[] windowfreq = new int[26];
 
         for (int i = 0; i < p.length(); i++) {
-            freqMap[p.charAt(i) - 'a']++;
+            char ch = p.charAt(i);
+            freq[ch - 'a']++;
         }
 
-        for (int r = 0; r < s.length(); r++) {
-            windowFreq[s.charAt(r) - 'a']++;
+        int left = 0;
+        for (int right = 0; right < s.length(); right++) {
+            char leftchar = s.charAt(right);
+            windowfreq[leftchar - 'a']++;
 
-            if (r - left + 1 > k) {
-                windowFreq[s.charAt(left) - 'a']--;
+            while (right - left + 1 > p.length()) {
+                windowfreq[s.charAt(left) - 'a']--;
                 left++;
             }
-
-            if (r - left + 1 == k) {
-                boolean same = true;
-                for (int i = 0; i < 26; i++) {
-                    if (freqMap[i] != windowFreq[i]) {
-                        same = false;
-                        break;
-
-                    }
-
-                }
-                if (same) {
-                    ans.add(left);
-                }
+            if (Arrays.equals(freq, windowfreq)) {
+                ans.add(left);
             }
         }
         return ans;
